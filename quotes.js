1da5a827cdb11b847733cc55ea6fadd877a6ab82
ncfisherim <ncfisherim@outlook.com>
2026-04-03 13:02:48 -0400

    adding another quote

--- quotes.js
+++ quotes.js
@@ -1,6 +1,6 @@
 const quotePairs = [
   {
-    theme: "impermanence",
+    theme: "Impermanence",
     quotes: [
       {
         text: "You have power over your mind, not outside events. Realize this and you will find strength.",
@@ -16,5 +16,25 @@ const quotePairs = [
       }
     ],
     reflection: "Where in your life are you trying to control something outside yourself?"
+  }
+  {
+    theme: "Faith",
+    quotes: [
+      {
+        text: "You know, I am enjoying two pensions. One is by the recommendation of your father, for whom I once worked in the railway " +
+        "office. The other is by the recommendation of my Heavenly Father, for whom I have conscientiously finished my earthly duties in life. " +
+        "A pension of fathomless peace—a reward for many years of deep meditation.",
+        author: "Swami Pranabananda, the \"Saint with Two Bodies\"",
+        tradition: "Hindu Yoga",
+        year: "~1905"
+      },
+      {
+        text: "The present moment is filled with joy and happiness. If you are attentive, you will see it",
+        author: "Thich Nhat Hanh",
+        tradition: "Vietnamese Buddhism",
+        year: "~2009"
+      }
+    ],
+    reflection: "Do the ebs and flows of your faith change your actions?"
   }
 ];
